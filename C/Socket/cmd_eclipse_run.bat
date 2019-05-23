96ac49ac519f1bdf62d1c6f1bb732f2018f421fb
Gerard KESSE <tiakagerard@hotmail.com>
2019-05-23 09:40:54 +0200

    Initial Commit

--- C/Socket/cmd_eclipse_run.bat
+++ C/Socket/cmd_eclipse_run.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set PATH=C:\Users\Admin\Downloads\x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0\mingw64\bin
+set PATH=C:\Users\gerard\Downloads\Gerard\x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0\mingw64\bin
 
 echo.
 code\GProject\Release\GProject.exe
