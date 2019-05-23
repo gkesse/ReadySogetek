918da3aa060d2db82bfbbdee6768e4cadb6e8968
Gerard KESSE <tiakagerard@hotmail.com>
2019-05-23 16:42:11 +0200

    Initial Commit

--- C/Socket/cmd_eclipse_start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set PATH=D:\Java\jdk-12\bin
-set PATH=C:\Users\gerard\Downloads\Gerard\eclipse-cpp-2019-06-M2-win32-x86_64\eclipse;%PATH%
-set PATH=C:\Users\gerard\Downloads\Gerard\x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0\mingw64\bin;%PATH%
-
-eclipse
