e66c0c7ceee5e94a43ce6e515220f2c671b5af70
Gerard KESSE <tiakagerard@hotmail.com>
2019-05-24 20:31:42 +0200

    Initial Commit

--- C/Socket/win/cmd_build.bat
+++ C/Socket/win/cmd_build.bat
@@ -1,3 +1,9 @@
-::call cmd_clean.bat
-call cmd_compile.bat
-call cmd_run.bat
+@echo off
+
+set PATH=C:\Users\Admin\Downloads\x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0\mingw64\bin
+
+echo.
+bin\GProject.exe
+echo.
+
+pause
--- C/Socket/win/cmd_build.sh
+++ C/Socket/win/cmd_build.sh
@@ -1,3 +1,9 @@
-./cmd_clean.sh
-./cmd_compile.sh
-./cmd_run.sh
+@echo off
+
+set PATH=C:\Users\Admin\Downloads\x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0\mingw64\bin
+
+echo.
+bin\GProject.exe
+echo.
+
+pause
